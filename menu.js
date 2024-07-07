504c0a61e1875757a4dffac6b86fcd9374db786f
Israel Sanchez Valle <44451779+brickjonas@users.noreply.github.com>
2024-07-06 23:09:19 -0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    function getBreakfastItems() {
+        const foodItem = 'menu/breakfast.json';
+        console.log('Fetching from:', foodItem); // Check if path is correct
+        fetch(foodItem)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                console.log('Data:', data); // Log the data to ensure it's fetched correctly
+                const foodCards = document.querySelector('.foodCards');
+                data.forEach(function(item) {
+                    const foodGridBox = document.createElement("div");
+                    foodGridBox.classList.add('foodCard');
+                    foodGridBox.innerHTML = `
+                        <h3>${item.name}</h3>
+                        <p>Price: ${item.cost}</p>
+                        <p>Ingredients: ${item.ingredients}</p>
+                        <p>Photo: ${item.photo}</p>
+                    `;
+                    foodCards.appendChild(foodGridBox);
+                });
+            })
+            .catch(function(error) {
+                console.error("Error fetching JSON data:", error);
+            });
+    }
+
+    getBreakfastItems();
+}
